1bf78f79081d13edf8c84715dc9b3f4c1bd309a3
Jair <jair.fehlauer@gmail.com>
2021-10-02 20:37:27 +0300

    First draft of message_handler

--- server.py
+++ server.py
@@ -7,83 +7,107 @@ import threading
 import signal
 import subprocess
 import sys
-from linux import *
+from utils import *
 
 os.system("clear")
 
-host = input("Server-Hostname: ")
-username = input("Username: ")
-oldpassword = input("Old Password: ")
-newpassword = input("New Password: ")
-verify_password = input("New Password: ")
-
-if newpassword != verify_password:	# Verify that the password is spelled correct
-	print("New Passwords dont match!!", file=sys.stderr)
-	exit(-1)
-
-process=subprocess.run(args=["smbpasswd", "-s", "-r", host, "-U", username], input=f"{oldpassword}\n{newpassword}\n{newpassword}\n".encode("utf-8"), stdout=subprocess.PIPE, stderr = subprocess.PIPE)
-print(process.args)
-print("stdout:")
-print(process.stdout.decode("utf-8"))
-print("stderr:")
-print(process.stderr.decode("utf-8"))
-print("returncode:")
-print(process.returncode)
+SAMBA_USERS_GROUP = "sambausers"
+CONFIG_FILE_PATH = "/etc/SambaRemote/config.config"
+EXECUTABLE_PATH = "/usr/bin/SambaRemote"
+VERSION = 1.0
 
 
-exit()
+Message_Types = {
+	"Command": "Command",
+	"User_List": "User_List",
+	"Open_Connection_Message": "Open_Connection_Message",
+	"Close_Connection_Message": "Close_Connection_Message"
+}
+Commands = {
+	"Change_Password": "Change_Password",
+	"Reboot": "Reboot",
+	"Shutdown": "Shutdown",
+	"Add_User": "Add_User",
+	"Delete_User": "Delete_User",
+	"Update_System": "Update_System"
+}
+User_Types = {
+	"Super_User": "SuperUser",
+	"Normal_User": "Normal_User"
+}
+
+
+message_handler = {
+	"Command": {
+		"Change_Password": change_samba_password,
+		"Shutdown": shutdown_system,
+		"Reboot": reboot_system
+	}
+
+}
+
+def terminate_handler():
+	"""
+		the handler, if the proccess gets the signal SIGTERM
+	"""
+	# Shutdown the connections with the clients(maybe send message, that the server will restart) and exit
+	pass
 
 user_list = get_linux_users_list()
 for user in user_list:
 	print(user)
 
 
+
 class client_handler(threading.Thread):
 	"""
 		Handles the socket-connection with the client and runs the requested commands/actions
 	"""
-	def __init__(self, client_sock, client_addr):
+	def __init__(self, client_sock:MySocket, client_addr):
 		self.client_sock = client_sock
 		self.client_addr = client_addr
 
-	def loop(self):
+	def run(self):
 		"""
 			handles connection and command execution
 		"""
-		pass
+
+		# open the connection and send the version
+		send_msg = {}
+		send_msg["MessageType"] = Message_Types["Open_Connection_Message"]
+		send_msg["Version"] = VERSION
+		self.client_sock.send(json.dumps(send_msg).encode(FORMAT))	# Load the message into a json-string and convert this string to a byte-sequence
+
+		try:
+			while True:
+				recv_msg = str(client_socket.recv(), FORMAT)
+				recv_msg = json.loads(recv_msg)
+				try:
+					if recv_msg["Message_Type"] == Message_Types["Command"]:
+						if recv_msg["Message"]["Command"] == Commands["Change_Password"]:
+							pass
+						elif recv_msg["Message"]["Command"] == Commands["Add_User"]:
+							pass
+					elif recv_msg["Message_Type"] == Message_Types["Close_Connection_Message"]:
+						print("the connection was closed by the client")
+				
+				except KeyError as err:
+					print("Value is missing in client-message:")
+					print(err.args)
+
+		
+		except BrokenPipeError as err:
+			print("Connection to the client is broken")
+		except...:
+			print("An Unknown error occurd")
+
 
 
 
 #client_handler_threads = list()
 
 
-class samba_remote_server:
-	def __init__(self, address: tuple, family=socket.AF_INET):
-		"""
-			*address* is build like this: ("listen_address for the server - empty it should listens to all adresses", listen_port)
-		"""
-		self.__server = socket.create_server(address, family=family)	# ceates a TCP-socket(SOCK_STREAM)
-		self.server_is_running = True
-	
-	def __del__(self):
-		self.__server.shutdown()
-		self.server_is_running = False
 
-	def loop(self):
-		"""
-			This function needs to be called in the main loop - it checks of a new client wants to connect and handles it!
-		"""
-		(client_sock, client_addr) = self.__server.accept()
-		c_handler = client_handler(client_sock, client_addr)
-		
-		c_handler_thread = threading.Thread(target=c_handler.loop, args=())
-		
-		client_handler_threads.append(c_handler_thread)
-		c_handler_thread.start()
-
-
-
-#server = samba_remote_server(("", 9090))
 
 
 #if __name__ == "__main__":
@@ -105,27 +129,27 @@ class samba_remote_server:
 
 PORT = 9090
 SERVER = ""
+FORMAT = "utf-8"
 ADDRESS = (SERVER, PORT)    # tuple
 
+
+if __name__ == "__main__":
+	pass
 
 print("The system supports IPv6?: " + str(socket.has_ipv6))  # Has IPv6?
 print("Platform supports both IPv4 and IPv6 socket stream connections: " + str(socket.has_dualstack_ipv6()))
 
-
-
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDRESS)
-server.listen()
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_server.bind(ADDRESS)
+socket_server.listen()
 
 
 
 while True:
 	print("Listening for client connections....")
-	(client_socket, client_addr) = server.accept()
+	(client_socket, client_addr) = socket_server.accept()
 	mysocket = MySocket(client_socket)
 
-	mysocket.header = 8192
 	send_data = input("Eingabe: ")
 	send_data_bytes = send_data.encode("utf-8")
 	mysocket.send(send_data_bytes)
@@ -134,13 +158,13 @@ while True:
 	
 	recv_data = mysocket.recv()
 	print(f"recived {len(recv_data)} bytes of data")
-	recv_data = recv_data.decode("utf-8")
+	#recv_data = recv_data.decode("utf-8")
 	print(recv_data)
 	if recv_data == "exit":
 		mysocket.shutdown(socket.SHUT_RDWR)
 		mysocket.close()
 		break
 
-server.shutdown(socket.SHUT_RDWR)
-server.close()
+socket_server.shutdown(socket.SHUT_RDWR)
+socket_server.close()
 
