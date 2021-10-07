f08a82ea8140de28242ae22a15419c615a53a9f3
Jair-F <jair.fehlauer@gmail.com>
2021-10-07 20:15:56 +0300

    Removed bloating - first need to impl. pwd-change

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ import subprocess
 import sys
 from utils import *
 
-os.system("clear")
+os.system("cls")
 
-SAMBA_USERS_GROUP = "sambausers"
+
 CONFIG_FILE_PATH = "/etc/SambaRemote/config.config"
 EXECUTABLE_PATH = "/usr/bin/SambaRemote"
 VERSION = 1.0
@@ -19,44 +19,20 @@ VERSION = 1.0
 
 Message_Types = {
 	"Command": "Command",
-	"User_List": "User_List",
-	"Open_Connection_Message": "Open_Connection_Message",
+	"ServerResponse": "ServerResponse",
 	"Close_Connection_Message": "Close_Connection_Message"
 }
 Commands = {
 	"Change_Password": "Change_Password",
-	"Reboot": "Reboot",
-	"Shutdown": "Shutdown",
-	"Add_User": "Add_User",
-	"Delete_User": "Delete_User",
-	"Update_System": "Update_System"
-}
-User_Types = {
-	"Super_User": "SuperUser",
-	"Normal_User": "Normal_User"
 }
 
 
 message_handler = {
 	"Command": {
-		"Change_Password": change_samba_password,
-		"Shutdown": shutdown_system,
-		"Reboot": reboot_system
+		"Change_Samba_Password": change_samba_password,
 	}
 
 }
-
-def terminate_handler():
-	"""
-		the handler, if the proccess gets the signal SIGTERM
-	"""
-	# Shutdown the connections with the clients(maybe send message, that the server will restart) and exit
-	pass
-
-user_list = get_linux_users_list()
-for user in user_list:
-	print(user)
-
 
 
 class client_handler(threading.Thread):
@@ -82,17 +58,30 @@ class client_handler(threading.Thread):
 			while True:
 				recv_msg = str(client_socket.recv(), FORMAT)
 				recv_msg = json.loads(recv_msg)
+				return_message = {}
 				try:
 					if recv_msg["Message_Type"] == Message_Types["Command"]:
 						if recv_msg["Message"]["Command"] == Commands["Change_Password"]:
-							pass
-						elif recv_msg["Message"]["Command"] == Commands["Add_User"]:
-							pass
+							username = recv_msg["Message"]["Username"]
+							old_password = recv_msg["Message"]["OldPassword"]
+							new_password = recv_msg["Message"]["NewPassword"]
+
+							(returncode, command_output) = change_samba_password(username, old_password, new_password)
+							if "NT_STATUS_LOGON_FAILURE" in command_output:
+								return_message[Message_Types["ServerResponse"]]["Response"] = "Either the Samba-Server is not running or your password/username is not correct"
+							else:
+								return_message[Message_Types["ServerResponse"]]["Response"] = command_output
+						
+						self.client_sock.send(return_message)	# Send a response to the client
+
+
 					elif recv_msg["Message_Type"] == Message_Types["Close_Connection_Message"]:
-						print("the connection was closed by the client")
+						print("the connection was closed by the client.")
+						self.client_sock.shutdown(socket.SHUT_RDWR)
+						self.client_sock.close()
 				
 				except KeyError as err:
-					print("Value is missing in client-message:")
+					print("A required parameter/value is missing in client-message:")
 					print(err.args)
 
 		
@@ -143,9 +132,19 @@ socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.bind(ADDRESS)
 socket_server.listen()
 
+def terminate_handler():
+	"""
+		the handler, if the proccess gets the signal SIGTERM
+	"""
+	# Shutdown the connections with the clients(maybe send message, that the server will restart) and exit
+	socket_server.shutdown(socket.SHUT_RDWR)
+	socket_server.close()
+	exit(0)
 
+terminate_handler = signal.signal(signal.SIGTERM, terminate_handler)
 
 while True:
+	
 	print("Listening for client connections....")
 	(client_socket, client_addr) = socket_server.accept()
 	mysocket = MySocket(client_socket)
