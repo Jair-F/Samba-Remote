7c29b8f349d05282971ca496ef2c36fd42eb93ad
Jair-F <jair.fehlauer@gmail.com>
2021-10-15 21:09:45 +0300

    Fixed typo

--- install_server.sh
+++ install_server.sh
@@ -8,11 +8,11 @@ fi
 
 echo "Copy files"
 
-mkidr /usr/bin/Samba_Remote
+mkdir /usr/bin/Samba_Remote
 
 install -o root server_modules server.py modules /usr/bin/Samba_Remote
 install -o root default_configs/samba_remote.service /etc/systemd/system
-	
+
 echo "Starting and enabling Samba_Remote service"
 systemctl enable SambaRemote.service
 systemctl start SambaRemote.service
