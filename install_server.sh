31df84dc8a72d712707259ffccc6eb3257098e01
Jair-F <jair.fehlauer@gmail.com>
2021-10-15 21:18:36 +0300

    Fixed install problems

--- install_server.sh
+++ install_server.sh
@@ -7,12 +7,16 @@ then
 fi
 
 echo "Copy files"
-
 mkdir /usr/bin/Samba_Remote
 
-install -o root server_modules server.py modules /usr/bin/Samba_Remote
-install -o root default_configs/samba_remote.service /etc/systemd/system
+cp -R server_modules modules /usr/bin/Samba_Remote
+cp server.py /usr/bin/Samba_Remote
+chown -R root /usr/bin/Samba_Remote
+
+cp default_configs/samba_remote.service /etc/systemd/system
+chown root /etc/systemd/system/samba_remote.service
+
 
 echo "Starting and enabling Samba_Remote service"
-systemctl enable SambaRemote.service
-systemctl start SambaRemote.service
+systemctl enable samba_remote.service
+systemctl start samba_remote.service
