a04aa0eaade8b9a201aeb3b7ce4089691d9177bd
Jair-F <jair.fehlauer@gmail.com>
2021-10-15 21:24:02 +0300

    Added copy config-File

--- install_server.sh
+++ install_server.sh
@@ -7,7 +7,8 @@ then
 fi
 
 echo "Copy files"
-mkdir /usr/bin/Samba_Remote
+mkdir /usr/bin/Samba_Remote	# Binary-Folder(Python-scripts)
+mkdir /etc/Samba_Remote		# Config-Folder
 
 cp -R server_modules modules /usr/bin/Samba_Remote
 cp server.py /usr/bin/Samba_Remote
@@ -15,6 +16,9 @@ chown -R root /usr/bin/Samba_Remote
 
 cp default_configs/samba_remote.service /etc/systemd/system
 chown root /etc/systemd/system/samba_remote.service
+
+cp default_configs/samba_remote.config /etc/Samba_Remote/
+chown -R root /etc/Samba_Remote/
 
 
 echo "Starting and enabling Samba_Remote service"
