8c5f3c9cb08fed73e7720d24cc8ad3e0d6cadccd
Jair-F <jair.fehlauer@gmail.com>
2021-10-15 21:07:53 +0300

    Added install_server script

---
+++ install_server.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ $UID -ne 0 ]
+then
+	echo "This script must to be run as root"
+	exit -1
+fi
+
+echo "Copy files"
+
+mkidr /usr/bin/Samba_Remote
+
+install -o root server_modules server.py modules /usr/bin/Samba_Remote
+install -o root default_configs/samba_remote.service /etc/systemd/system
+	
+echo "Starting and enabling Samba_Remote service"
+systemctl enable SambaRemote.service
+systemctl start SambaRemote.service
