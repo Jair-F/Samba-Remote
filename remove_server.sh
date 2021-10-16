5aa8ab258cf2d3cd867294a9b1085f2e5e7d9a2a
Jair-F <jair.fehlauer@gmail.com>
2021-10-16 13:49:26 +0300

    added path vars

--- remove_server.sh
+++ remove_server.sh
@@ -6,12 +6,18 @@ then
 	exit -1
 fi
 
+BIN_DIR="/usr/bin/Samba_Remote"
+CONF_DIR="/etc/Samba_Remote"
+CONF_FILE_PATH="$CONF_DIR/samba_remote.config"
+SYSTEMD_SERVICE_DIR="/etc/systemd/system"
+SYSTEMD_SERVICE_FILE_PATH="$SYSTEMD_SERVICE_DIR/samba_remote.service"
+
 echo "Stoping Samba_Remote-Server"
 systemctl stop samba_remote.service
 systemctl disable samba_remote.service
 
 echo "Deleting files"
 
-rm -rfd /usr/bin/Samba_Remote
-rm /etc/systemd/system/samba_remote.service
-rm -rfd /etc/Samba_Remote/
+rm -rfd $BIN_DIR
+rm $SYSTEMD_SERVICE_FILE_PATH
+rm -rfd $CONF_DIR
